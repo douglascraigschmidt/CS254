522097ebdab4380a6f3c99b33522f7cfae8a64c6
douglascraigschmidt <douglas.craig.schmidt@gmail.com>
2022-01-17 19:23:16 -0600

    foo

--- assignment4a/app/src/androidTest/java/edu/vandy/app/ui/screens/main/Assignment_3A_InstrumentedTests.kt
+++
@@ -1,9 +0,0 @@
-package edu.vandy.app.ui.screens.main
-
-import edu.vandy.simulator.managers.beings.BeingManager.Factory.Type.COMPLETION_SERVICE
-import edu.vandy.simulator.managers.palantiri.PalantiriManager.Factory.Type.REENTRANT_LOCK_HASH_MAP_SIMPLE_SEMAPHORE
-
-class Assignment_3A_InstrumentedTests : InstrumentedTests() {
-    override val beingManager = COMPLETION_SERVICE
-    override val palantirManager = REENTRANT_LOCK_HASH_MAP_SIMPLE_SEMAPHORE
-}
